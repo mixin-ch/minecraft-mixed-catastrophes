a1a2bcd1bb6b911ab8ac02d1f8b6bb83d5c618f7
Dokzentir <mischa.iwasaki@gmail.com>
2021-05-28 19:58:05 +0200

    achievements blueprint setup

--- src/main/java/ch/mixin/mixedCatastrophes/mixedAchievements/MixedAchievementsManager.java
+++ src/main/java/ch/mixin/mixedCatastrophes/mixedAchievements/MixedAchievementsManager.java
@@ -1,16 +1,16 @@
 package ch.mixin.mixedCatastrophes.mixedAchievements;
 
 import ch.mixin.mixedAchievements.api.AchievementApi;
-import ch.mixin.mixedAchievements.blueprint.AchievementBlueprintFolderElement;
-import ch.mixin.mixedAchievements.blueprint.AchievementBlueprintLeafElement;
-import ch.mixin.mixedAchievements.blueprint.AchievementItemSetup;
-import ch.mixin.mixedAchievements.blueprint.AchievementSetBlueprint;
+import ch.mixin.mixedAchievements.blueprint.*;
 import ch.mixin.mixedAchievements.main.MixedAchievementsPlugin;
 import ch.mixin.mixedCatastrophes.main.MixedCatastrophesPlugin;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
 
 import javax.naming.ServiceUnavailableException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class MixedAchievementsManager {
     private boolean active = false;
@@ -34,17 +34,27 @@ public class MixedAchievementsManager {
 
     private AchievementSetBlueprint makeAchievementSetBlueprint() {
         AchievementSetBlueprint achievementSetBlueprint = new AchievementSetBlueprint(
-                new AchievementItemSetup(Material.NETHER_STAR, ChatColor.of("#FFFFFF") + "Mixed Catastrophes", 1)
+                new AchievementItemSetup(Material.NETHER_STAR, ChatColor.of("#FFFFFF") + "Mixed Catastrophes", 1, new ArrayList<>())
                 , "MixedCatastrophes"
                 , "Mixed Catastrophes");
 
-        achievementSetBlueprint.getSubAchievementBlueprintElementMap().put(1, new AchievementBlueprintLeafElement(
-                new AchievementItemSetup(Material.OAK_PLANKS, ChatColor.of("#7F00FF") + "Test", 4)
+        fillBlueprintAchievements(achievementSetBlueprint.getSubAchievementBlueprintElementMap());
+        return achievementSetBlueprint;
+    }
+
+    private void fillBlueprintAchievements(HashMap<Integer, AchievementBlueprintElement> subAchievementBlueprintElementMap) {
+        subAchievementBlueprintElementMap.put(slot(1, 1), new AchievementBlueprintLeafElement(
+                new AchievementItemSetup(Material.OAK_PLANKS, ChatColor.of("#7F00FF") + "Test", 4, new ArrayList<>(Arrays.asList(
+                        "Do something."
+                )))
                 , "test"
                 , "test"
                 , 4
         ));
-        return achievementSetBlueprint;
+    }
+
+    private int slot(int row, int col) {
+        return 9 * (row - 1) + (col - 1);
     }
 
     public boolean isActive() {
