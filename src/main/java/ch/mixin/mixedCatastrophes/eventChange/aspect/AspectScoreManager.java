1a5bccdd9fa4dfef11091a628c9473f1f5d8c376
Dokzentir <mischa.iwasaki@gmail.com>
2021-05-29 22:00:17 +0200

    fixed Scoreboard (finally)

--- src/main/java/ch/mixin/mixedCatastrophes/eventChange/aspect/AspectScoreManager.java
+++ src/main/java/ch/mixin/mixedCatastrophes/eventChange/aspect/AspectScoreManager.java
@@ -5,11 +5,9 @@ import ch.mixin.mixedCatastrophes.helperClasses.Constants;
 import ch.mixin.mixedCatastrophes.main.MixedCatastrophesPlugin;
 import ch.mixin.mixedCatastrophes.metaData.PlayerData;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
+import org.bukkit.scoreboard.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,7 +16,8 @@ import java.util.UUID;
 
 public class AspectScoreManager {
     private static final List<AspectType> aspectOrder;
-    private static final List<UUID> playerIdList = new ArrayList<>();
+    //private final List<UUID> playerIdList = new ArrayList<>();
+    private final HashMap<UUID, Scoreboard> playerMap = new HashMap<>();
 
     static {
         aspectOrder = new ArrayList<>();
@@ -31,7 +30,7 @@ public class AspectScoreManager {
         aspectOrder.add(AspectType.Greyhat_Debt);
     }
 
-    //    private final ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
+    private final ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
     private final MixedCatastrophesPlugin plugin;
 
     public AspectScoreManager(MixedCatastrophesPlugin plugin) {
@@ -40,20 +39,19 @@ public class AspectScoreManager {
 
     public void updateScoreboard(Player player) {
         UUID playerId = player.getUniqueId();
-        int playerIndex = playerIdList.indexOf(player.getUniqueId());
-
-        if (playerIndex == -1) {
-            playerIdList.add(playerId);
-            playerIndex = playerIdList.size() - 1;
-        }
-
         PlayerData pcd = plugin.getMetaData().getPlayerDataMap().get(playerId);
         HashMap<AspectType, Integer> aspects = pcd.getAspects();
 
         int dreamCooldown = pcd.getDreamCooldown();
         int antiLighthouseTimer = pcd.getAntiLighthouseTimer();
 
-        Scoreboard scoreboard = player.getScoreboard();
+        Scoreboard scoreboard = playerMap.get(playerId);
+
+        if (scoreboard == null) {
+            scoreboard = scoreboardManager.getNewScoreboard();
+            playerMap.put(playerId, scoreboard);
+        }
+
         Objective objective = scoreboard.getObjective("Aspects");
 
         if (objective == null) {
@@ -68,18 +66,18 @@ public class AspectScoreManager {
             int value = aspects.get(aspectType);
 
             if (value > 0) {
-                makeScore(scoreboard, objective, playerIndex, index, Constants.AspectThemes.get(aspectType).getColor(), aspectType.getLabel(), value);
+                makeScore(scoreboard, objective, index, Constants.AspectThemes.get(aspectType).getColor(), aspectType.getLabel(), value);
                 index++;
             }
         }
 
         if (dreamCooldown > 0) {
-            makeScore(scoreboard, objective, playerIndex, index, Constants.DreamThemes.get(DreamType.SereneDreams).getColor(), "Dreamless", dreamCooldown, "s");
+            makeScore(scoreboard, objective, index, Constants.DreamThemes.get(DreamType.SereneDreams).getColor(), "Dreamless", dreamCooldown, "s");
             index++;
         }
 
         if (antiLighthouseTimer > 0) {
-            makeScore(scoreboard, objective, playerIndex, index, Constants.AspectThemes.get(AspectType.Terror).getColor(), "Red Eye", antiLighthouseTimer, "s");
+            makeScore(scoreboard, objective, index, Constants.AspectThemes.get(AspectType.Terror).getColor(), "Red Eye", antiLighthouseTimer, "s");
             index++;
         }
 
@@ -91,12 +89,12 @@ public class AspectScoreManager {
         player.setScoreboard(scoreboard);
     }
 
-    private void makeScore(Scoreboard scoreboard, Objective objective, int playerId, int index, ChatColor chatColor, String label, int value) {
-        makeScore(scoreboard, objective, playerId, index, chatColor, label, value, "");
+    private void makeScore(Scoreboard scoreboard, Objective objective, int index, ChatColor chatColor, String label, int value) {
+        makeScore(scoreboard, objective, index, chatColor, label, value, "");
     }
 
-    private void makeScore(Scoreboard scoreboard, Objective objective, int playerId, int index, ChatColor chatColor, String label, int value, String valueUnit) {
-        String teamIndexString = playerId + ":" + Integer.toString(index);
+    private void makeScore(Scoreboard scoreboard, Objective objective, int index, ChatColor chatColor, String label, int value, String valueUnit) {
+        String teamIndexString = Integer.toString(index);
         String scoreString = getScoreString(index);
         Team team = scoreboard.getTeam(teamIndexString);
 
@@ -122,5 +120,4 @@ public class AspectScoreManager {
 
         return scoreString.toString();
     }
-
 }
