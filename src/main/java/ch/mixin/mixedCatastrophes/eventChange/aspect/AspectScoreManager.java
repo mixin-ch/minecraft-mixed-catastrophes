de64e5492a26de3380fb582abb3af612e21146b1
Dokzentir <mischa.iwasaki@gmail.com>
2021-05-29 14:17:33 +0200

    Fixed Scoreboard

--- src/main/java/ch/mixin/mixedCatastrophes/eventChange/aspect/AspectScoreManager.java
+++ src/main/java/ch/mixin/mixedCatastrophes/eventChange/aspect/AspectScoreManager.java
@@ -1,10 +1,10 @@
 package ch.mixin.mixedCatastrophes.eventChange.aspect;
 
-import ch.mixin.mixedCatastrophes.metaData.PlayerData;
 import ch.mixin.mixedCatastrophes.helperClasses.Constants;
 import ch.mixin.mixedCatastrophes.main.MixedCatastrophesPlugin;
-import org.bukkit.Bukkit;
+import ch.mixin.mixedCatastrophes.metaData.PlayerData;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.*;
 
@@ -29,50 +29,64 @@ public class AspectScoreManager {
         Objective objective = scoreboard.registerNewObjective("Aspects", "dummy", ChatColor.GRAY + "Aspects");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        int hierarchy = 2 + 1 + aspects.size();
+        int index = 0;
         boolean hasTimers = false;
 
         if (dreamCooldown > 0) {
-            makeScore(objective, hierarchy, ChatColor.DARK_AQUA, "Dreamless", dreamCooldown, "s");
-            hierarchy--;
+            makeScore(scoreboard, objective, index, ChatColor.DARK_AQUA, "Dreamless", dreamCooldown, "s");
+            index++;
             hasTimers = true;
         }
 
         if (antiLighthouseTimer > 0) {
-            makeScore(objective, hierarchy, Constants.AspectThemes.get(AspectType.Terror).getColor(), "Red Eye", antiLighthouseTimer, "s");
-            hierarchy--;
+            makeScore(scoreboard, objective, index, Constants.AspectThemes.get(AspectType.Terror).getColor(), "Red Eye", antiLighthouseTimer, "s");
+            index++;
             hasTimers = true;
         }
 
         if (hasTimers) {
-            emptyScore(objective, hierarchy);
-            hierarchy--;
+            emptyScore(objective, index);
+            index++;
         }
 
         for (AspectType aspectType : AspectType.values()) {
             int value = aspects.get(aspectType);
 
             if (value > 0) {
-                makeScore(objective, hierarchy, Constants.AspectThemes.get(aspectType).getColor(), aspectType.getLabel(), value);
-                hierarchy--;
+                makeScore(scoreboard, objective, index, Constants.AspectThemes.get(aspectType).getColor(), aspectType.getLabel(), value);
+                index++;
             }
         }
 
         player.setScoreboard(scoreboard);
     }
 
-    private void makeScore(Objective objective, int hierarchy, ChatColor chatColor, String label, int value) {
-        makeScore(objective, hierarchy, chatColor, label, value, "");
+    private void makeScore(Scoreboard scoreboard, Objective objective, int index, ChatColor chatColor, String label, int value) {
+        makeScore(scoreboard, objective, index, chatColor, label, value, "");
     }
 
-    private void makeScore(Objective objective, int hierarchy, ChatColor chatColor, String label, int value, String valueUnit) {
-        String text = chatColor + label + ": " + value + valueUnit;
-        Score score = objective.getScore(text);
-        score.setScore(hierarchy);
+    private void makeScore(Scoreboard scoreboard, Objective objective, int index, ChatColor chatColor, String label, int value, String valueUnit) {
+        String indexString = Integer.toString(index);
+        Team team = scoreboard.registerNewTeam(indexString);
+        team.setSuffix(chatColor + label + ": " + value + valueUnit);
+        StringBuilder scoreString = new StringBuilder();
+
+        for (char c : indexString.toCharArray()) {
+            scoreString.append(ChatColor.translateAlternateColorCodes('&', "&" + c));
+        }
+
+        team.addEntry(scoreString.toString());
+        objective.getScore(scoreString.toString()).setScore(0);
     }
 
-    private void emptyScore(Objective objective, int hierarchy) {
-        Score score = objective.getScore("");
-        score.setScore(hierarchy);
+    private void emptyScore(Objective objective, int index) {
+        StringBuilder scoreString = new StringBuilder();
+
+        for (char c : Integer.toString(index).toCharArray()) {
+            scoreString.append(ChatColor.translateAlternateColorCodes('&', "&" + c));
+        }
+
+        Score score = objective.getScore(scoreString.toString());
+        score.setScore(0);
     }
 }
